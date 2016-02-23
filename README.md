755c3108b40f1f078b8de1b3b6fc5aa4306a6550
Sachin Goyal <sgoyal6@L-SB8M1HEG8W-M.local>
2016-02-22 19:38:50 -0800

    Add Kafka-Storm example

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
 # storm-examples
+
+
+## Examples in this repository
+
+Each example is completely contained in a single file in **examples/storm/storm\_hello\_world/**
+
+1. HelloWorld.java - Basic example where a bold just adds an exclamation mark on its input word.
+2. ExclamationWithMetrics.java - Shows the usage of metrics.
+3. KafkaReader.java - Shows how to use the storm-kafka "plugin" with kafka.
+
+
+## Installing Storm
+
+As per the guide at [Setting-up-a-Storm-cluster](http://storm.apache.org/documentation/Setting-up-a-Storm-cluster.html), first step is to [install zookeeper](http://zookeeper.apache.org/doc/r3.3.3/zookeeperStarted.html#sc_InstallingSingleMode) which is simply downloading, unzipping, setting up some properties and running the ZK server.
+
+You can then run the ZK command-line-interface to interact with your ZK:
+
+```bash
+
+$ bin/zkCli.sh -server 127.0.0.1:2181
+  help
+  ls /
+
+```
+
+Once ZK is running, play with some of the ZK four letter commands:
+
+```bash
+
+$ echo ruok | nc 127.0.0.1 2181
+imok
+
+```
+
+Make copies of the downloaded storm:
+
+```bash
+
+mv storm-0.10.0    storm-0.10.0.nimbus
+cp -rf storm-0.10.0.nimbus    storm-0.10.0.supervisor1
+cp -rf storm-0.10.0.nimbus    storm-0.10.0.supervisor2
+
+```
+
+Start nimbus in one shell
+
+```bash
+
+cd storm-0.10.0.nimbus/
+bin/storm nimbus
+
+```
+ 
+Start supervisor and the UI in the other shell
+
+```bash
+
+cd  storm-0.10.0.supervisor1
+bin/storm supervisor &
+bin/storm ui &
+
+```
+
+ 
+Then navigate to http://localhost:8080/index.html to see that storm is up and running.
+
+
+
+## Building an uber jar with maven shade plugin
+
+This examples code uses the maven-shaden-plugin to build an uber jar (i.e. a jar with all the dependency jars).
+
+That is required because the regular storm distribution does not contain the storm-kafka jars.
+And we need to either put them in storm's lib/ directory or build an uber jar.
+
+But storm does complain about "multiple defaults.yaml resources" when it sees the Storm jars bundled in the topology jar file. Hence we remove it by using scope "provided" for storm-core libraries.
+
+maven-assembly-plugin might be useful too.
+
+
+
+## Running the examples
+
+1. Build jar using `mvn clean package`
+2. Copy target/storm\_hello\_world-0.0.1-SNAPSHOT.jar to a convienient location.
+3. Run storm as `bin/storm jar ../\*.jar examples.storm.storm\_hello\_world.HelloWorld`
+
---
+++ src/main/java/examples/storm/storm_hello_world/KafkaReader.java
@@ -0,0 +1,112 @@
+package examples.storm.storm_hello_world;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.AuthorizationException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+import storm.kafka.BrokerHosts;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <pre>
+ * Example showing how to read from Kafka and print to the console.
+ * Usage:
+ *   bin/storm jar \
+ *       [path-to-jar] \
+ *       examples.storm.storm_hello_world.KafkaReader
+ *       [ZK-Connection-String]
+ *       [Kafka-Topic-Name]
+ *       [ZK-Root-For-Consumer-Offsets]
+ * </pre>
+ */
+public class KafkaReader
+{
+	@SuppressWarnings("serial")
+	public static class PrintlnBolt extends BaseRichBolt
+	{
+		OutputCollector _collector;
+		private static final Logger logger = LoggerFactory.getLogger(PrintlnBolt.class);
+		
+		public void prepare(Map stormConf, TopologyContext context, OutputCollector collector)
+		{
+			_collector = collector;
+		}
+
+		public void execute(Tuple tuple) {
+			//_collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+			_collector.ack(tuple);
+			System.out.println("Printing tuple with toString(): " + tuple.toString());
+		    System.out.println("Printing tuple with getString(): " + tuple.getString(0));
+		    logger.info("Logging tuple with logger: " + tuple.getString(0));
+		}
+
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {}
+	}
+
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException
+    {
+		if (args == null || args.length != 4)
+		{
+			System.err.println ("Wrong args. \n" +
+					"Usage: bin/storm jar [path-to-jar] \n" + 
+					"examples.storm.storm_hello_world.KafkaReader] \n" +
+					"[ZK-Connection-String] \n" +
+					"[Kafka-Topic-Name] \n" +
+					"[ZK-Root]");
+			return;
+		}
+		String _zkConnString = args[1];
+		String _topicName = args[2];
+		String _zkRootForConsumerOffsets = args[3];
+		
+    	BrokerHosts zkHosts = new ZkHosts(_zkConnString);
+    	SpoutConfig spoutConfig = new SpoutConfig(zkHosts, _topicName, _zkRootForConsumerOffsets, UUID.randomUUID().toString());
+    	spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+    	KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+
+    	TopologyBuilder builder = new TopologyBuilder();
+    	// For every setSpout() / setBolt() methods, there is an integer argument
+    	// called 'parallelism_hint' which denotes the number of tasks that 'should' be assigned to
+    	// execute this spout. Each task will run on a thread in a process somewhere around the cluster.
+        builder.setSpout("kafkaSpout", kafkaSpout, 1);
+        builder.setBolt("exclaim1", new PrintlnBolt(), 3).shuffleGrouping("kafkaSpout");
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        if (args != null && args.length > 0)
+        {
+          conf.setNumWorkers(3);
+          StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        }
+        else
+        {
+          LocalCluster cluster = new LocalCluster();
+          cluster.submitTopology("test", conf, builder.createTopology());
+          Utils.sleep(10000);
+          cluster.killTopology("test");
+          cluster.shutdown();
+        }
+    }
+}
